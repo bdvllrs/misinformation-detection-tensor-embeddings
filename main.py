b60550d79738c13c1ecd20d63279ca3c75390253
Benamira Adrien <adrien.benamira@supelec.fr>
2018-10-03 18:07:36 +0200

    implémentation FaBP

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ from ArticleTensor import ArticleTensor
 from kNN import embedding_matrix_2_kNN
 from utils import solve
 
-num_nearest_neighbours = 5
-rank_parafac_decomposition = 10
-num_fake_articles = 10
-num_real_articles = 10
-num_unknown_labels = 10
+num_nearest_neighbours = 2
+rank_parafac_decomposition = 3
+num_fake_articles = 2
+num_real_articles = 2
+num_unknown_labels = 2
 size_word_co_occurrence_window = 5
 use_frequency = False
-homophily = 1
 
 assert num_fake_articles + num_real_articles > num_nearest_neighbours, "Can't have more neighbours than nodes!"
-assert 0 <= homophily <= 1, "Homophily should between 0 and 1."
+
 
 articleTensor = ArticleTensor('../Dataset/fakenewsdata1/Public Data')
 articleTensor.get_articles('Buzzfeed Political News Dataset', number_fake=num_fake_articles,
@@ -24,7 +23,7 @@ _, _, C = ArticleTensor.get_parafac_decomposition(tensor, rank=rank_parafac_deco
 
 graph = embedding_matrix_2_kNN(C, k=num_nearest_neighbours).toarray()
 
-beliefs = solve(graph, labels, homophily)
+beliefs = solve(graph, labels)
 
 print(beliefs)
 print(all_labels)
