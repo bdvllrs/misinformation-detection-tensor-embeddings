9d9c38565f36a2db03afc8f5f3700f18de1cd063
Benamira Adrien <adrien.benamira@supelec.fr>
2018-10-04 10:14:19 +0200

    add logs

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
 from ArticleTensor import ArticleTensor
 from kNN import embedding_matrix_2_kNN
 from utils import solve
+import time
 
 num_nearest_neighbours = 2
 rank_parafac_decomposition = 3
-num_fake_articles = 2
-num_real_articles = 2
-num_unknown_labels = 2
+num_fake_articles = 10
+num_real_articles = 10
+num_unknown_labels = 10
 size_word_co_occurrence_window = 5
 use_frequency = False
 
 assert num_fake_articles + num_real_articles > num_nearest_neighbours, "Can't have more neighbours than nodes!"
 
-
+#print get tensor
+debut=time.time()
 articleTensor = ArticleTensor('../Dataset/fakenewsdata1/Public Data')
 articleTensor.get_articles('Buzzfeed Political News Dataset', number_fake=num_fake_articles,
                            number_real=num_real_articles)
 articleTensor.build_word_to_index()
 tensor, labels, all_labels = articleTensor.get_tensor(window=size_word_co_occurrence_window, num_unknown=num_unknown_labels,
                                           use_frequency=use_frequency)
+fin=time.time()
+print("get tensor done",fin-debut)
 _, _, C = ArticleTensor.get_parafac_decomposition(tensor, rank=rank_parafac_decomposition)
-
+fin2=time.time()
+print("decomposition done",fin2-fin)
 graph = embedding_matrix_2_kNN(C, k=num_nearest_neighbours).toarray()
-
+fin3=time.time()
+print("KNN done",fin3-fin2)
+#classe  b(i){> 0, < 0} means i ∈ {“+”, “-”}
 beliefs = solve(graph, labels)
+fin4=time.time()
+print("FaBP done",fin4-fin3)
 
-print(beliefs)
-print(all_labels)
 
+
+print("return float belief", beliefs)
+beliefs[beliefs > 0] = 1
+beliefs[beliefs < 0] = -1
+print("return int belief", beliefs)
+print("labels correct",all_labels)
+print("labels a completer", labels)
 
 
