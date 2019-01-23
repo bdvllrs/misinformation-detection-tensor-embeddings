51aebcf60c7e24981701508448c32b80017cf4cc
AdrienBenamira <benamira.adrien@gmail.com>
2019-01-23 10:19:59 +0100

    Change config and add multi embedding graph

--- utils/Config.py
+++ utils/Config.py
@@ -1,61 +1,36 @@
-__author__ = "Benjamin Devillers (bdvllrs)"
-__credits__ = ["Benjamin Devillers (bdvllrs)"]
-__license__ = "GPL"
-
-import json
 import os
+import yaml
+
+
+def update_config(conf, new_conf):
+    for item in new_conf.keys():
+        if type(new_conf[item]) == dict and item in conf.keys():
+            conf[item] = update_config(conf[item], new_conf[item])
+        else:
+            conf[item] = new_conf[item]
+    return conf
 
 
 class Config:
-    def __init__(self, file=None, config=None, args=None, is_set=True):
-        if file is not None:
-            filepath = os.path.abspath(os.path.join(os.path.curdir, file))
-            files = os.listdir(filepath)
-            files.remove('config.default.json')
-            config_filepath = os.path.abspath(os.path.join(filepath, 'config.default.json'))
-            with open(config_filepath, 'r') as f:
-                self.config = json.load(f)
-            for file in files:
-                if file[-4:] == 'json':
-                    config_filepath = os.path.abspath(os.path.join(filepath, file))
-                    with open(config_filepath, 'r') as f:
-                        self.config = {**self.config, **json.load(f)}
-        elif config is not None:
-            self.config = config
-        if args is not None:
-            for arg, value in vars(args).items():
-                if arg not in self.config.keys() or value is not None:
-                    self.config[arg] = value
-        self._is_set = is_set
-        self.default_config = self.config.copy()
+    def __init__(self, path=None, config=None):
+        self.__is_none = False
+        self.__data = config if config is not None else {}
+        if path is not None:
+            self.__path = os.path.abspath(os.path.join(os.curdir, path))
+            with open(os.path.join(self.__path, "default.yaml"), "rb") as default_config:
+                self.__data.update(yaml.load(default_config))
+            for config in sorted(os.listdir(self.__path)):
+                if config != "default.yaml" and config[-4:] in ["yaml", "yml"]:
+                    with open(os.path.join(self.__path, config), "rb") as config_file:
+                        self.__data = update_config(self.__data, yaml.load(config_file))
 
     def set(self, key, value):
-        self.config[key] = value
-
-    def reset(self, key):
-        self.config[key] = self.default_config[key]
-
-    def is_set(self, key=None):
-        """
-        Check if the value exists
-        """
-        is_set = True
-        if self._is_set and key is not None:
-            is_set = key in self.config.keys()
-        return self._is_set and is_set
-
-    def get(self, item):
-        if self.is_set(item):
-            if type(self.config[item]) == dict:
-                return Config(config=self.config[item])
-            return self.config[item]
-        return Config(is_set=False)
-
-    def __str__(self):
-        return str(self.config)
+        self.__data[key] = value
 
     def __getattr__(self, item):
-        return self.get(item)
+        if type(self.__data[item]) == dict:
+            return Config(config=self.__data[item])
+        return self.__data[item]
 
     def __getitem__(self, item):
-        return self.get(item)
+        return self.__data[item]
